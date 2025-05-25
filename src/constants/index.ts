6ede57957cf105dbfab1904fbf2e865caa8d59b9
judahwon <hwondaa@gmail.com>
2025-05-25 22:40:03 +0900

    [형식] : website title 디키에서 Diki로 수정한다

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const dikiMetadata = {
-  title: '디키',
+  title: 'Diki',
   description: '디키(Diki, 데이터 위키)는 데이터 사이언티스트, 애널리스트, 엔지니어를 위한 전문 용어사전입니다. 디키는 데이터 직무 종사자들이 필요한 지식을 쉽게 찾고 이해할 수 있도록 도와드립니다.',
   url: 'https://diki.kr',
   thumbnailURL: 'https://diki.kr/thumbnail.png',
