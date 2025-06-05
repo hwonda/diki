af7faa299605fc43d35effb8c5f6f3a124c0056e
judahwon <hwondaa@gmail.com>
2025-06-05 22:00:14 +0900

    [테스트] : diki.kr -> www.diki.kr 리디렉션 빌드 설정 추가한다

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'media.licdn.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'diki.kr',
+          },
+        ],
+        destination: 'https://www.diki.kr/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
