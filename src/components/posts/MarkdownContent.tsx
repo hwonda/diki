301e5fb1940099fdb5136440d2a621c9f07eaafa
judahwon <hwondaa@gmail.com>
2025-11-30 15:51:31 +0900

    [리팩토링] : 마크다운(개념)섹션 이스케이프 쌍따옴표 처리한다

--- src/components/posts/MarkdownContent.tsx
+++ src/components/posts/MarkdownContent.tsx
@@ -74,11 +74,12 @@ function parseMarkdownSegment(segment: string) {
   </div>
 `);
 
-  // Bold / Italic / Inline code / 링크
+  // Bold / Italic / Inline code / 링크 / 이스케이프 쌍따옴표
   html = html.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>');
   html = html.replace(/\*([^*]+)\*/g, '<em>$1</em>');
   html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
   html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
+  html = html.replace(/\\"/g, '"');
 
   return html;
 }
